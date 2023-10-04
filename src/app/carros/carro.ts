3bfa88dd4ced85bb9cea1fb0648dbc13fdc216e2
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-10-04 20:57:05 -0300

    models

---
+++ src/app/carros/carro.ts
@@ -0,0 +1,8 @@
+export class Carro {
+    modelo!: string;
+    ano!: number;
+    constructor(modelo:string, ano:number){
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
---
+++ src/app/livros/livro.ts
@@ -0,0 +1,9 @@
+export class Livro {
+    titulo!: string;
+    autor!: string; 
+
+    constructor(titulo:string, autor:string){
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+}
