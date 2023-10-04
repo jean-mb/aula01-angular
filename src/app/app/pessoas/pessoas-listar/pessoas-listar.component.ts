037389074afdba8e8b385c72c30d78cd1d0c55db
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-10-03 21:16:58 -0300

    menu ok

--- src/app/app/pessoas/pessoas-listar/pessoas-listar.component.ts
+++ src/app/app/pessoas/pessoas-listar/pessoas-listar.component.ts
@@ -10,8 +10,15 @@ export class PessoasListarComponent {
   pessoas: Pessoa[] = [];
   constructor(){
     let pessoa1: Pessoa = new Pessoa("Jean", 18);
-    let pessoa2: Pessoa = new Pessoa("Vinicius", 23);
-    let pessoa3: Pessoa = new Pessoa("Jean", 19);
-    this.pessoas.push(pessoa1,pessoa2,pessoa3);
+    let pessoa2: Pessoa = new Pessoa("Maria", 25);
+    let pessoa3: Pessoa = new Pessoa("Carlos", 30);
+    let pessoa4: Pessoa = new Pessoa("Ana", 19);
+    let pessoa5: Pessoa = new Pessoa("Pedro", 53);
+    let pessoa6: Pessoa = new Pessoa("Lúcia", 62);
+    let pessoa7: Pessoa = new Pessoa("Miguel", 15);
+    let pessoa8: Pessoa = new Pessoa("Isabel", 80);
+    let pessoa9: Pessoa = new Pessoa("Ricardo", 28);
+    let pessoa10: Pessoa = new Pessoa("Sofia", 90);
+    this.pessoas.push(pessoa1,pessoa2,pessoa3, pessoa4, pessoa5, pessoa6, pessoa7, pessoa8, pessoa9, pessoa10);
   }
 }
