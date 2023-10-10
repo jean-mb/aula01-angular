7e644a035f63e67e6554a0ec767efbe9c3f05c7e
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-10-10 20:03:53 -0300

    fix: nomeclatura de service

---
+++ src/app/services/pessoa.service.spec.ts
@@ -0,0 +1,7 @@
+import { PessoaService } from './pessoa.service';
+
+describe('PessoaService', () => {
+  it('should create an instance', () => {
+    expect(new PessoaService()).toBeTruthy();
+  });
+});
