299e3c4630202db94ae8a814a383bd4a6ee13b11
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-10-10 19:30:57 -0300

    fix: correcao de imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PessoasListarComponent } from './pessoas/pessoas-listar/pessoas-listar.component';
-import { IndexComponent } from './layout/index/index.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { PessoasDetailsComponent } from './pessoas/pessoas-details/pessoas-details.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { LoginComponent } from './sistema/login/login.component';
-import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
-import { CarrosListarComponent } from './carros/carros-listar/carros-listar.component';
-import { CarrosDetailsComponent } from './carros/carros-details/carros-details.component';
-import { LivrosDetailsComponent } from './livros/livros-details/livros-details.component';
-import { LivrosListarComponent } from './livros/livros-listar/livros-listar.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PessoasListarComponent } from './components/pessoas/pessoas-listar/pessoas-listar.component';
+import { IndexComponent } from './components/layout/index/index.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { PessoasDetailsComponent } from './components/pessoas/pessoas-details/pessoas-details.component';
+import { AdminHomeComponent } from './components/admin/admin-home/admin-home.component';
+import { CarrosDetailsComponent } from './components/carros/carros-details/carros-details.component';
+import { CarrosListarComponent } from './components/carros/carros-listar/carros-listar.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { LivrosDetailsComponent } from './components/livros/livros-details/livros-details.component';
+import { LivrosListarComponent } from './components/livros/livros-listar/livros-listar.component';
+import { LoginComponent } from './components/sistema/login/login.component';
 
 
 @NgModule({
